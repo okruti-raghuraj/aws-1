7b8491390fa2ef370531e512aef9b7a2505eec57
Raghuraj Singh Solanki <rsolanki@okruti.com>
2022-04-19 11:27:16 +0530

    basic change

--- first-aws-cdk/first-aws-cdk-app/bin/first-aws-cdk-app.ts
+++ first-aws-cdk/first-aws-cdk-app/bin/first-aws-cdk-app.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+// import * as cdk from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk';
 import { FirstAwsCdkAppStack } from '../lib/first-aws-cdk-app-stack';
 
 const app = new cdk.App();
@@ -11,7 +12,7 @@ new FirstAwsCdkAppStack(app, 'FirstAwsCdkAppStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
