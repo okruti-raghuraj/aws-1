be1ec9e9202d78cc1df75c05cea69f085d624de0
Raghuraj Singh Solanki <rsolanki@okruti.com>
2022-04-20 12:05:27 +0530

    2nd commit

--- first-aws-cdk/first-aws-cdk-app/lib/first-aws-cdk-app-stack.ts
+++ first-aws-cdk/first-aws-cdk-app/lib/first-aws-cdk-app-stack.ts
@@ -4,6 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as path from 'path';
+import { Table } from '@aws-cdk/aws-dynamodb';
 
 export class FirstAwsCdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,21 +13,30 @@ export class FirstAwsCdkAppStack extends cdk.Stack {
     // The code that defines your stack goes here
     const apiGateway = new apigateway.RestApi(this , 'APIGW' , {});
     apiGateway.root.addMethod('GET');
+    apiGateway.root.addMethod('POST');
 
     const userResource = apiGateway.root.addResource('users');
+
+    const tableUsers = new dynamodb.Table(this , 'Users' , {
+      partitionKey:{ name: 'id', 
+                      type: dynamodb.AttributeType.STRING 
+                    },
+      sortKey :{ name :'task_id', 
+                  type : dynamodb.AttributeType.STRING
+                },
+    });
     
     const lambdaListUsers = new lambda.Function(this , 'lambdalistusers' , {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname,'lambda-handler'))
+      code: lambda.Code.fromAsset(path.join(__dirname,'lambda-handler')),
+      environment:{
+        Table_Name_User : tableUsers.tableName,
+      }
     });
 
     userResource.addMethod('GET' , new apigateway.LambdaIntegration(lambdaListUsers));
     userResource.addMethod('PUT');
-    const tableUsers = new dynamodb.Table(this , 'Users' , {
-      partitionKey:{ name: 'id', 
-      type: dynamodb.AttributeType.STRING },
-    });
 
     tableUsers.grantReadData(lambdaListUsers);
     // example resource
