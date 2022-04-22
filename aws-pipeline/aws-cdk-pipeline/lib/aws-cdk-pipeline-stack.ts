f24f2c368da7db78ea77336f72b913fc6586676b
Raghuraj Singh Solanki <rsolanki@okruti.com>
2022-04-22 18:02:27 +0530

    aws-pipeline-initial-commit

---
+++ aws-pipeline/aws-cdk-pipeline/lib/aws-cdk-pipeline-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+//import * as cdk from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import { CodePipeline ,  CodePipelineSource , ShellStep } from 'aws-cdk-lib/pipelines';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsCdkPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    const pipeline = new CodePipeline(this , 'Pipeline' ,{
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('synth' , {
+        input: CodePipelineSource.gitHub('Raghuraj Singh Solanki/SMS','aws-pipeline'),
+        commands:['npm ci' ,
+                  'npm run build',
+                  'npx cdk synth']
+      }),
+    });
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsCdkPipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
